b5b37c5f438338cca71f6d4845bb0c87697d24e7
Stanislav Dzundza <stas.dzundza@gmail.com>
2020-11-28 22:19:43 +0200

    add methods for communication with db

--- OracleSqlORM/__main__.py
+++ OracleSqlORM/__main__.py
@@ -1,4 +1,13 @@
 # Python to Oracle SQL ORM
+from py2sql import *
+
 
 if __name__ == '__main__':
     print('Python to Oracle SQL ORM Framework')
+    orm = Py2SQL()
+    db_credentials = DbCredentials('system', 'OraPasswd1', '40.117.92.106/cdb1')
+    orm.db_connect(db_credentials)
+    orm.db_engine()
+    orm.db_name()
+    # orm.db_tables()
+    orm.db_disconnect()
